601e8d335646507a2a8b4f5872ac75abc5c5909d
sabimbi447 <asarejonas447@gmail.com>
2022-07-04 01:15:15 +0100

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     '''Replace an element in a copied list at a paticular poistion'''
-    if idx < 0 or idx > (len(my_list) -1):
+    if idx < 0 or idx > (len(my_list)):
         return (my_list)
 
     copy = [x for x in my_list]
