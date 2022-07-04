b3688c8d9f890d53b75fe9c1885a1e7656ef14f9
sabimbi447 <asarejonas447@gmail.com>
2022-07-04 12:18:06 +0100

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 def divisible_by_2(my_list=[]):
-    multiples = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            multiples.append(True)
-        else:
-            multiples.append(False)
-    return (multiples)
-
+    new_list = []
+    if my_list:
+        for elm in my_list:
+            new_list.append(False if elm % 2 else True)
+    return new_list
