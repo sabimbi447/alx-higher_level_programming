7ba320c9ecb24d814d41eb7e84472657fd0ceca3
sabimbi447 <asarejonas447@gmail.com>
2024-01-10 08:19:17 +0100

    Task 1 fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-# 2-is_same_class.py
-"""Defines a class-checking function."""
+"""this module creates a subclass of a list"""
 
 
-def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a given class.
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is exactly an instance of a_class - True.
-        Otherwise - False.
-    """
-    return type(obj) is a_class
+class MyList(list):
+    """this class subclasses a list object"""
+
+    def print_sorted(self):
+        """this function prints a list in sorted order"""
+        this = self[:]
+        this.sort()
+        print(this)
+        
